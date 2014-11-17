006eac3e33d3ea7fb83597491aa733455f2dc0a2
Marco Krage <marco@my-azur.de>
2014-11-17 18:54:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,22 @@
 var fs = require('fs');
 var path = require('path');
 var request = require('request');
-var base64 = require('base64');
+var async = require('async');
 
 // Globals
 var OUT_DIR = path.join(__dirname, 'pictures');
 var imageIds = require('./imageIds.json');
+
+if ( !fs.existsSync("./pictures") ) {
+  fs.mkdirSync("./pictures");
+}
+
+
+var queue = async.queue(download, '4');
+queue.drain = function() {
+  console.log("\nAll done!");
+}
+queue.push(imageIds);
 
 
 function writePhoto(photo) {
@@ -34,7 +45,7 @@ function writePhoto(photo) {
     );
 }
 
-imageIds.forEach(function(id) {
+function download(id, cb) {
     var url = "https://www.gstatic.com/prettyearth/"+id+".json";
     var options = {
         url: url,
@@ -43,14 +54,17 @@ imageIds.forEach(function(id) {
     request.get(options, function(err, body, parsed) {
         if(err) {
             console.error("Failed to get [", id, "] because of", err);
+            cb();
             return;
         }
 
         try {
             writePhoto(parsed);
+            cb();
         } catch(errx) {
             console.error("Failed to write [", id, "] because of", errx);
+            cb();
             return;
         }
     });
-});
+}
